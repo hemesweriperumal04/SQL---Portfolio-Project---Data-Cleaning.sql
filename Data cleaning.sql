4758182ac27db94a930806b361aa471e0fe8ba0d
hemesweriperumal04 <hemesweriperumal04@gmail.com>
2026-03-05 14:51:30 +0530

    Update Data cleaning.sql

--- Data cleaning.sql
+++ Data cleaning.sql
@@ -32,4 +32,24 @@ from
 staging_layoffs; 
 
 --- then we will find duplicate
-select 
+
+select * from 
+            (select company, industry, total_laid_off, 'date',
+row_number () over (partition by company, industry, total_laid_off, 'date') 
+as r_n 
+from layoffs) duplicates
+where r_n > 1;
+--- Now we need to confirm if these true entries or not 
+select * from staging_layoffs
+where company = 'oda'; --- here you can entries are true but only company & industry & total_laid_off, date is duplicate but rest other true entries
+
+--- now we need to find real duplicates with all coloumn names
+
+select * 
+  from 
+  (select company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
+  row_number () over (partition by company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions)
+  as row_numb
+from staging_layoffs
+)duplicates
+where row_numb > 1;
